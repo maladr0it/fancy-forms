91c626bf936cabb34b60bc98ed49a7785c9e9106
maladr0it <bradford.lachlan@gmail.com>
2020-06-24 16:19:41 +0900

    convert to typescript

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Fancy Forms</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>yo</h1>
+      <style jsx>{``}</style>
+      <style jsx global>{``}</style>
+    </>
+  );
+}
